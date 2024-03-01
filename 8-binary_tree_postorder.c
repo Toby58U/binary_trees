1c1a7ae64481189851515186e41029319a98e809
Toby58U <tobennaumeh001@gmail.com>
2024-03-01 14:31:09 -0800

    Binary trees

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -12,10 +12,10 @@ void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 	{
 		return; /* Do nothing if tree or func is NULL */
 	}
-	
-	/* Recursively traverse the left and right subtrees in post-order */
-	binary_tree_postorder(tree->left, func);
-	binary_tree_postorder(tree->right, func);
-	/* Call the function for the current node */
+	else
+	{
+		binary_tree_postorder(tree->left, func);
+		binary_tree_postorder(tree->right, func);
+	}
 	func(tree->n);
-}
+}	
--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -7,16 +7,20 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
+	size_t l = 0;
+	size_t r = 0;
+
 	if (tree == NULL)
 	{
 		return (0);
 	}
-
-	size_t left_height = 0, right_height = 0;
-
-	left_height = tree->left ? 1 + binary_tree_height(tree->left) : 0;
-	right_height = tree->right ? 1 + binary_tree_height(tree->right) : 0;
-
-	return ((left_height > right_height) ? left_height : right_height);
-
+	else
+	{
+		if (tree)
+		{
+			l = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+			r = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+		}
+		return ((l > r) ? l : r);
+	}
 }
